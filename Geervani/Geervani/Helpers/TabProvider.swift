0273911fc6573cf9627169046500c79f572c6e2f
Prasanna Ramaswamy <prasanna.hr@gmail.com>
2017-11-29 19:56:46 +0530

    Changing tabbar icons

--- Geervani/Geervani/Helpers/TabProvider.swift
+++ Geervani/Geervani/Helpers/TabProvider.swift
@@ -19,27 +19,27 @@ class TabProvider {
             if (moduleName == subhashitaModule) {
 
                 let subhashitaViewController = SubhashitaController(nibName: "SubhashitaController", bundle: nil)
-                let navController = navigationControllerWithRootController(subhashitaViewController, title: "Subhashita", imageName: "icon_tabBar_subhashita", tag: index)
+                let navController = navigationControllerWithRootController(subhashitaViewController, title: "Subhashita", imageName: "subhashita_icon", tag: index)
                 viewControllers.append(navController)
             }
             else if (moduleName == topicModule) {
 
                 let topicController = TopicController(nibName: "TopicController", bundle: nil)
-                let navController = navigationControllerWithRootController(topicController, title: "Topics", imageName: "icon_tabBar_topic", tag: index)
+                let navController = navigationControllerWithRootController(topicController, title: "Topics", imageName: "topics_icon", tag: index)
                 viewControllers.append(navController)
             }
 
             else if (moduleName == dictionaryModule) {
 
                 let dictionaryController = DictionaryController(nibName: "DictionaryController", bundle: nil)
-                let navController = navigationControllerWithRootController(dictionaryController, title: "Dictionary", imageName: "icon_tabBar_dictionary", tag: index)
+                let navController = navigationControllerWithRootController(dictionaryController, title: "Dictionary", imageName: "dictionary_icon", tag: index)
                 viewControllers.append(navController)
             }
 
             else if (moduleName == infoModule) {
 
                 let infoController = InfoController(nibName: "InfoController", bundle: nil)
-                let navController = navigationControllerWithRootController(infoController, title: "Info", imageName: "icon_tabBar_info", tag: index)
+                let navController = navigationControllerWithRootController(infoController, title: "Info", imageName: "info_icon", tag: index)
                 viewControllers.append(navController)
             }
 
